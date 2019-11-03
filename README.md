96d59c40e378015944b0697a38a40ed8ce945f65
elleon003 <ngrandison@ymail.com>
2019-11-03 10:10:37 -0500

    UI Game v0.8 - unfinished

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 *A JavaScript game created for the Odin Project Curriculum.*
 
-A basic game that uses alerts to display results. More versions to come!
+V1 - Game based entirely in alerts (found in *basicAlertVersion.html*)
+
+V2 - Same game logic with a UI
+
+Things learned in this project:
+
+- Always write out your plan before you start coding. It's the only way to stay on task
+- Think about everything you want to happen every time an action is taken (*like clicking a button*). That really helped me refactor my original code to work in a site.
+- I really don't think I'm destined for any kind of design work. I wanted to do more, but I find myself having more fun making things happen than making things look great. Mad respect to those that design.
+- This is an unfinished project. I was never able to figure out how to get images to display for each side's choices, but I'm banging my head up against a wall at the moment, so I'm going to move on and come back.
--- script.js
+++ script.js
@@ -1,49 +1,114 @@
 let computerScore = 0;
 let playerScore = 0;
-let roundResult;
-const playerOptions = document.querySelectorAll('button');
-const gameResults = document.querySelectorAll('#gameResults');
 
+
+const playerOptions = document.querySelectorAll('.selections');
+const gameDisplay = document.querySelector('#gameDisplay');
+const userScoreDisplay = document.querySelector('#userScore');
+const computerScoreDisplay = document.querySelector('#computerScore');
+
+const choiceImages = document.querySelectorAll('.itemDisplay');
+const rock = choiceImages[0];
+const paper = choiceImages[1];
+const scissors = choiceImages[2];
+
+
+const userCounter = document.createElement('P');
+const computerCounter = document.createElement('P');
+userScoreDisplay.appendChild(userCounter);
+computerScoreDisplay.appendChild(computerCounter);
+userScoreDisplay.appendChild(playerImage);
+computerScoreDisplay.appendChild(computerImage);
+
+
+const finalScoreDisplay = document.createElement('P');
+const newGameButton = document.createElement('Button');
+newGameButton.innerText = "New Game?";
+const h2 = document.querySelector('H2');
+const finalScore = document.querySelector('#finalScore');
 
 function computerPlay() {
     // Allows computer to randomly select rock, paper or scissors for game play
-    let options = ['Rock', 'Paper', 'Scissors'];
+    let options = ['rock', 'paper', 'scissors'];
     let selection = options[Math.floor(Math.random() * options.length)];
     return selection;
 }
 
-function gameRound(playerSelection, computerSelection) {
+function playerComparison(playerSelection, computerSelection) {
     // All possible outcomes in a game round
     if (playerSelection == computerSelection) {
-        roundResult = "It's a draw!";
-    } else if ((playerSelection == 'Rock' && computerSelection == 'Paper')
-        || (playerSelection == 'Scissors' && computerSelection == 'Rock')
-        || (playerSelection == 'Paper' && computerSelection == 'Scissors')) {
-        roundResult = `You lose! ${computerSelection} beats ${playerSelection}`;
-    } else if ((playerSelection == 'Paper' && computerSelection == 'Rock')
-        || (playerSelection == 'Rock' && computerSelection == 'Scissors')
-        || (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
-        roundResult = `You win! ${playerSelection} beats ${computerSelection}`;
-    }
-    alert(roundResult);
-    if (roundResult.includes("win")) {
-        playerScore++;
-    } else if (roundResult.includes("lose")) {
-        computerScore++;
+        return;
+    } else if ((playerSelection == 'rock' && computerSelection == 'paper')
+        || (playerSelection == 'scissors' && computerSelection == 'rock')
+        || (playerSelection == 'paper' && computerSelection == 'scissors')) {
+        return computerScore += 1;
+    } else if ((playerSelection == 'paper' && computerSelection == 'rock')
+        || (playerSelection == 'rock' && computerSelection == 'scissors')
+        || (playerSelection == 'scissors' && computerSelection == 'paper')) {
+        return playerScore += 1;
     }
 }
 
-function finalScore() {
+
+function displayChoice(playerSelection, computerSelection){
+    if (playerSelection == 'rock'){
+        playerImage = rock;
+    } else if (playerSelection == 'paper') {
+        playerImage = paper;
+    } else if (playerSelection == 'scissors') {
+        playerImage = scissors;
+    }
+    if (computerSelection == 'rock') {
+        computerImage = rock;
+    } else if (computerSelection == 'paper') {
+        computerImage = paper;
+    } else if (computerSelection == 'scissors') {
+        computerImage = scissors;
+    }
+}   
+
+function gamePlay(playerSelection, computerSelection) {
+    playerComparison(playerSelection, computerSelection);
+    displayChoice(playerSelection, computerSelection);    
+    userCounter.innerText = playerScore;
+    computerCounter.innerText = computerScore;
+    if (playerScore == 3 || computerScore == 3){
+        endGame();
+    }
+}
+
+function endGame() {
     if (playerScore > computerScore) {
-        alert(`You win!\nYour score: ${playerScore}\nComputer Score: ${computerScore}`);
+        finalScoreDisplay.innerText = `You win!\nYour score: ${playerScore}\nComputer Score: ${computerScore}`;
     } else {
-        alert(`You lose!\nYour score: ${playerScore}\nComputer Score: ${computerScore}`);
+        finalScoreDisplay.innerText = `You lose!\nYour score: ${playerScore}\nComputer Score: ${computerScore}`;
     }
+    finalScore.appendChild(finalScoreDisplay);
+    playerOptions.forEach((selection) => {
+        selection.style.display = 'none';
+    });
+    finalScore.appendChild(newGameButton);
+    newGameButton.style.display = 'inline';
+    h2.style.display = 'none';
 }
 
-// Links the button selection to game play
+// Links the button selection for game play
 playerOptions.forEach((selection) => {
     selection.addEventListener('click', (e) => {
-        gameRound(selection.innerText, computerPlay());
+        gamePlay(selection.id, computerPlay());
     });
+});
+
+// Restart game
+newGameButton.addEventListener('click',() => {
+    newGameButton.style.display = 'none';
+    playerOptions.forEach((selection) => {
+        selection.style.display = 'inline';
+    });
+    computerScore = 0;
+    playerScore = 0;
+    userCounter.innerText = playerScore;
+    computerCounter.innerText = computerScore;
+    finalScore.innerText = " ";
+    h2.style.display = 'inline';
 });
